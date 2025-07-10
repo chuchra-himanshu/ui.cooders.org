aa4dcf50606328d74f1afea31faac8e7d7f9feff
Himanshu Chuchra <code@himanshuchuchra.com>
2025-07-10 19:37:32 +0530

    Design Footer Component under UI Components

--- src/layouts/StudioLayout.tsx
+++ src/layouts/StudioLayout.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { Header } from "../components";
+import { Footer, Header } from "../components";
 
 const StudioLayout: React.FC = () => {
   return (
     <section className="h-screen bg-background-primary flex flex-col justify-between">
       <Header />
       <Outlet />
+      <Footer />
     </section>
   );
 };
