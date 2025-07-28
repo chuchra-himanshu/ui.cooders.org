13233c30dd2ceeba5d79c59842229ff357800f28
Himanshu Chuchra <code@himanshuchuchra.com>
2025-07-28 23:23:42 +0530

    Add Background Gradiant to Auth Module

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -7,11 +7,19 @@ const AuthLayout: React.FC = () => {
 
   return (
     <section className="h-screen bg-background-primary flex flex-col justify-between pt-2">
-      <section className="flex h-[85px] items-center justify-between px-6 ">
+      <div
+        className="absolute inset-0 z-0
+          [background-size:37.5px_37.5px]
+          [background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)] 
+          dark:[background-image:linear-gradient(to_right,#202020_1px,transparent_1px),linear-gradient(to_bottom,#202020_1px,transparent_1px)]"
+      />
+      <section className="flex h-[85px] items-center justify-between px-6 z-20">
         <Logo classes="w-[85px] h-[85px]" />
         <AuthLayoutButton title="SKIP" clickListener={() => navigate("/")} />
       </section>
-      <Outlet />
+      <section className="z-10">
+        <Outlet />
+      </section>
     </section>
   );
 };
